561824ff6d749c120d20da2f947a3fa0dc71dc76
Meni Yakove <myakove@gmail.com>
2024-02-20 08:34:38 +0200

    Add OWNERS file

--- app/reporter.py
+++ app/reporter.py
@@ -66,9 +66,6 @@ def main(months, aws_profiles, slack_webhook_url, config):
     end = datetime.today().strftime("%Y-%m-01")
 
     for aws_profile in aws_profiles:
-        _aws_profile = os.environ["AWS_PROFILE"] = (
-            aws_profile if aws_profile else "default"
-        )
         cost_explorer = CostExplorer(
             granularity="MONTHLY",
             start=start,
@@ -76,9 +73,10 @@ def main(months, aws_profiles, slack_webhook_url, config):
             dimensions=["SERVICE"],
             metrics="UnblendedCost",
             threshold=1.0,
+            aws_profile=aws_profile,
         )
         total_cost = cost_explorer.get_cost_and_usage_total()
-        msg = f"Profile {_aws_profile}:\n\t"
+        msg = f"Profile {aws_profile}:\n\t"
         for values in total_cost.values():
             for month, total in values.items():
                 msg += f"Date: {month}: {total}$\n\t"
@@ -87,8 +85,6 @@ def main(months, aws_profiles, slack_webhook_url, config):
 
         if slack_webhook_url:
             send_slack_message(message=msg, webhook_url=slack_webhook_url)
-
-        os.unsetenv("AWS_PROFILE")
 
 
 if __name__ == "__main__":
