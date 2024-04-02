e3d0dfbd344c93eec0edfcc04d695d601bcdbf9c
Meni Yakove <myakove@gmail.com>
2024-04-02 15:04:42 +0300

    Add cron support

--- app/reporter.py
+++ app/reporter.py
@@ -1,19 +1,27 @@
 import json
-from datetime import datetime
+import datetime
 import os
 import boto3
 import requests
 from botocore.client import ClientError
 from flask import Flask
+from flask.logging import default_handler
 from pyaml_env import parse_config
 import calendar
+import croniter
+from simple_logger.logger import get_logger
+from multiprocessing import Process
+import time
+
 
 FLASK_APP = Flask("AWS Cost Reporter")
+FLASK_APP.logger.removeHandler(default_handler)
+FLASK_APP.logger.addHandler(get_logger(FLASK_APP.logger.name).handlers[0])
 
 
 def send_slack_message(message, webhook_url):
     slack_data = {"text": message}
-    print(f"Sending message to slack: {message}")
+    FLASK_APP.logger.info(f"Sending message to slack: {message}")
     response = requests.post(
         webhook_url,
         data=json.dumps(slack_data),
@@ -25,21 +33,26 @@ def send_slack_message(message, webhook_url):
         )
 
 
-@FLASK_APP.route("/")
-def reporter():
+def update_cost_reporter(config_data=None):
     msg = ""
     total_cost = {}
-    config_data = parse_config(os.getenv("AWS_COST_REPORTER_CONFIG", "accounts.yaml"))
+    config_data = config_data or parse_config(
+        os.getenv("AWS_COST_REPORTER_CONFIG", "accounts.yaml")
+    )
     slack_webhook_url = config_data.get("slack-webhook-url")
     app_extrenal_url = config_data.get("app-external-url")
 
-    _today = datetime.today()
-    this_month_start = datetime(_today.year, _today.month, 1).strftime("%Y-%m-%d")
-    this_month_end = datetime.today().strftime("%Y-%m-%d")
+    _today = datetime.datetime.today()
+    this_month_start = datetime.datetime(_today.year, _today.month, 1).strftime(
+        "%Y-%m-%d"
+    )
+    this_month_end = datetime.datetime.today().strftime("%Y-%m-%d")
 
     _last_month = _today.month - 1
-    last_month_start = datetime(_today.year, _last_month, 1).strftime("%Y-%m-%d")
-    last_month_end = datetime(
+    last_month_start = datetime.datetime(_today.year, _last_month, 1).strftime(
+        "%Y-%m-%d"
+    )
+    last_month_end = datetime.datetime(
         _today.year, _last_month, calendar.monthrange(_today.year, _last_month)[1]
     ).strftime("%Y-%m-%d")
 
@@ -66,7 +79,7 @@ def reporter():
                 Metrics=["AmortizedCost"],
             )
         except ClientError as exp:
-            print(f"Failed to get cost for {account}: {exp}")
+            FLASK_APP.logger.info(f"Failed to get cost for {account}: {exp}")
             continue
 
         _this_month_total_cost_data = this_month_cost["ResultsByTime"][0]["Total"][
@@ -90,22 +103,54 @@ def reporter():
         msg += (
             f"{account}:\n"
             f"\t[{this_month_start}/{this_month_end}] {float(_this_month_total_cost): .2f}{_unit_symbol}\n"
-            f"\t[{last_month_start}/{last_month_end}]{float(_last_month_total_cost): .2f}{_unit_symbol}"
+            f"\t[{last_month_start}/{last_month_end}]{float(_last_month_total_cost): .2f}{_unit_symbol}\n"
         )
 
-    if slack_webhook_url:
-        slack_msg = f"{app_extrenal_url}\n{msg}"
+    if msg and slack_webhook_url:
+        slack_msg = f"""
+```{msg}
+To force update send run curl {app_extrenal_url}
+```
+"""
+        FLASK_APP.logger.info(f"Sending message to slack: {slack_msg}")
         send_slack_message(message=slack_msg, webhook_url=slack_webhook_url)
 
     return msg
 
 
+def run_in_background(cron, config_data):
+    FLASK_APP.logger.info("Starting cost reporter in background")
+    while True:
+        now = datetime.datetime.now()
+        next_run = cron.get_next(datetime.datetime)
+        delta = next_run - now
+        if delta.total_seconds() > 0:
+            time.sleep(delta.total_seconds())
+            try:
+                update_cost_reporter(config_data=config_data)
+            except Exception as exp:
+                FLASK_APP.logger.info(f"Failed to update cost reporter: {exp}")
+
+
+@FLASK_APP.route("/update")
+def webhook_update():
+    return update_cost_reporter()
+
+
 def main():
+    config_data = parse_config(os.getenv("AWS_COST_REPORTER_CONFIG", "accounts.yaml"))
+    if cron := config_data.get("cron"):
+        cron = croniter.croniter(cron, datetime.datetime.now())
+        proc = Process(
+            target=run_in_background, kwargs={"cron": cron, "config_data": config_data}
+        )
+        proc.start()
+
     FLASK_APP.logger.info(f"Starting {FLASK_APP.name} app")
     FLASK_APP.run(
         port=5000,
         host="0.0.0.0",
-        use_reloader=True,
+        use_reloader=False,
     )
 
 
