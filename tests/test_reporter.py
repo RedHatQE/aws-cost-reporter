8c1a7a227bd44393a48c4bc382eb0463e4b57251
Omri Bar-Haim <obarhaim@redhat.com>
2024-04-21 13:29:23 +0300

    Creating pytest environment for AWS cost reporter
    applying the following changes:
    1. Adding pytest dependencies to pyproject.toml (updating poetry.lock as well)
    2. Applying pytest addopts & markers config
    3. Adding tox config with extra poetry pytest flows
    4. Fixing reporter logging, re-using dates
    5. More changes to test_reporter module

--- tests/test_reporter.py
+++ tests/test_reporter.py
@@ -1,45 +1,109 @@
+import os
 import pytest
-from unittest.mock import patch, MagicMock
-from app.reporter import send_slack_message
+from unittest.mock import patch, Mock
+
+import yaml
+
+from app.reporter import update_cost_reporter, get_current_and_previous_months_dates
 
 
 @pytest.fixture
-def mock_requests_post():
-    with patch("app.reporter.requests.post") as mock:
-        yield mock
+def aws_static_costs():
+    return {"current_month": 700, "previous_month": 800}
 
 
 @pytest.fixture
-def mock_flask_logger_error():
-    with patch("app.reporter.FLASK_APP.logger.error") as mock:
-        yield mock
+def aws_accounts_file():
+    aws_accounts_filename = os.path.join(f"{os.getcwd()}/tests", "test_accounts.yaml")
+    os.environ["AWS_COST_REPORTER_CONFIG"] = aws_accounts_filename
+
+    return aws_accounts_filename
+
+
+@pytest.fixture
+def aws_account_names(aws_accounts_file):
+    with open(aws_accounts_file, "r") as fd:
+        aws_accounts_data = yaml.safe_load(stream=fd)
+        _aws_account_names = aws_accounts_data["accounts"].keys()
+
+        assert _aws_account_names, "Failed to get AWS account names to report costs"
+
+        yield _aws_account_names
+
+
+@pytest.fixture
+def expected_cost_report_message(aws_static_costs, aws_account_names):
+    this_month_start, this_month_end, last_month_start, last_month_end = (
+        get_current_and_previous_months_dates()
+    )
+    cost_report_message = ""
+    for aws_account_name in aws_account_names:
+        cost_report_message += (
+            f"{aws_account_name}:\n\t[{this_month_start}/{this_month_end}]{float(aws_static_costs["current_month"]): .2f}$\n"
+            f"\t[{last_month_start}/{last_month_end}]{float(aws_static_costs["previous_month"]): .2f}$\n"
+        )
+
+    assert cost_report_message, "Failed to create AWS cost report message"
+
+    return cost_report_message
+
+
+@pytest.fixture
+def mock_boto3_client(aws_static_costs):
+    with patch("boto3.client") as mock:
+        mocked_client = Mock()
+        mocked_client.get_cost_and_usage.side_effect = [
+            {
+                "ResultsByTime": [
+                    {
+                        "Total": {
+                            "NetUnblendedCost": {
+                                "Amount": aws_static_costs["current_month"],
+                                "Unit": "USD",
+                            }
+                        }
+                    }
+                ]
+            },
+            {
+                "ResultsByTime": [
+                    {
+                        "Total": {
+                            "NetUnblendedCost": {
+                                "Amount": aws_static_costs["previous_month"],
+                                "Unit": "USD",
+                            }
+                        }
+                    }
+                ]
+            },
+        ]
+        mock.return_value = mocked_client
+        yield mocked_client
 
 
 @pytest.mark.slack_report
-def test_send_slack_message_success(mock_requests_post):
-    mock_response = MagicMock()
-    mock_response.status_code = 200
-    mock_requests_post.return_value = mock_response
-    send_slack_message("Test message", "http://fakeurl.com")
-    mock_requests_post.assert_called_once_with(
-        "http://fakeurl.com",
-        data='{"text": "test message"}',
-        headers={"Content-Type": "application/json"},
+def test_aws_cost_report(
+    aws_accounts_file, expected_cost_report_message, mock_boto3_client
+):
+    this_month_start, this_month_end, last_month_start, last_month_end = (
+        get_current_and_previous_months_dates()
     )
 
+    actual_cost_report_message = update_cost_reporter()
 
-@pytest.mark.slack_report
-def test_send_slack_message_failure(mock_requests_post, mock_flask_logger_error):
-    mock_response = MagicMock()
-    mock_response.status_code = 400
-    mock_response.text = "Bad Request"
-    mock_requests_post.return_value = mock_response
-    send_slack_message("test message", "http://fakeurl.com")
-    mock_requests_post.assert_called_once_with(
-        "http://fakeurl.com",
-        data='{"text": "test message"}',
-        headers={"Content-Type": "application/json"},
+    mock_boto3_client.get_cost_and_usage.assert_any_call(
+        TimePeriod={"Start": this_month_start, "End": this_month_end},
+        Granularity="MONTHLY",
+        Metrics=["NetUnblendedCost"],
     )
-    mock_flask_logger_error.assert_called_once_with(
-        f"Request to slack returned an error {mock_response.status_code} with the following message: {mock_response.text}"
+    mock_boto3_client.get_cost_and_usage.assert_any_call(
+        TimePeriod={"Start": last_month_start, "End": last_month_end},
+        Granularity="MONTHLY",
+        Metrics=["NetUnblendedCost"],
+    )
+
+    assert actual_cost_report_message == expected_cost_report_message, (
+        f"AWS cost report message does not match the expected.\n"
+        f"Actual: {actual_cost_report_message}, expected: {expected_cost_report_message}"
     )
