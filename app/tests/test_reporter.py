2f96fd52842f66450d995425915dedea371800de
Omri Bar-Haim <obarhaim@redhat.com>
2024-05-02 12:11:25 +0300

    fix PR issues

--- app/tests/test_reporter.py
+++ app/tests/test_reporter.py
@@ -1,21 +1,17 @@
 import os
-from unittest.mock import MagicMock
-from pytest_mock.plugin import MockerFixture
-
 import pytest
 from pyaml_env import parse_config
-from typing import Dict, List
 
 from app.reporter import update_cost_reporter, get_current_and_previous_months_dates
 
 
 @pytest.fixture
-def aws_static_costs() -> Dict[str, int]:
+def aws_static_costs():
     return {"current_month": 700, "previous_month": 800}
 
 
 @pytest.fixture
-def aws_accounts_file() -> str:
+def aws_accounts_file():
     aws_accounts_filename = "app/tests/manifests/test_accounts.yaml"
     os.environ["AWS_COST_REPORTER_CONFIG"] = aws_accounts_filename
 
@@ -23,7 +19,7 @@ def aws_accounts_file() -> str:
 
 
 @pytest.fixture
-def aws_account_names(aws_accounts_file: str) -> List[str]:
+def aws_account_names(aws_accounts_file):
     return [
         account_name
         for account_name, account_data in parse_config(path=aws_accounts_file)[
@@ -33,28 +29,25 @@ def aws_account_names(aws_accounts_file: str) -> List[str]:
 
 
 @pytest.fixture
-def expected_cost_report_message(
-    aws_static_costs: Dict[str, int], aws_account_names: List[str]
-) -> str:
+def expected_cost_report_message(aws_static_costs, aws_account_names):
     this_month_start, this_month_end, last_month_start, last_month_end = (
         get_current_and_previous_months_dates()
     )
-    return (
-        f"{aws_account_names[0]}:\n"
-        f"\t[{this_month_start}/{this_month_end}]{float(aws_static_costs['current_month']): .2f}$\n"
-        f"\t[{last_month_start}/{last_month_end}]{float(aws_static_costs['previous_month']): .2f}$\n"
-        f"{aws_account_names[1]}:\n"
-        f"\t[{this_month_start}/{this_month_end}]{float(aws_static_costs['current_month']): .2f}$\n"
-        f"\t[{last_month_start}/{last_month_end}]{float(aws_static_costs['previous_month']): .2f}$\n"
-    )
+    return f"""{aws_account_names[0]}:
+\t[{this_month_start}/{this_month_end}]{float(aws_static_costs['current_month']): .2f}$
+\t[{last_month_start}/{last_month_end}]{float(aws_static_costs['previous_month']): .2f}$
+{aws_account_names[1]}:
+\t[{this_month_start}/{this_month_end}]{float(aws_static_costs['current_month']): .2f}$
+\t[{last_month_start}/{last_month_end}]{float(aws_static_costs['previous_month']): .2f}$
+"""
 
 
 @pytest.fixture
 def mocked_boto3_client(
-    mocker: MockerFixture,
-    aws_static_costs: Dict[str, int],
-    aws_account_names: List[str],
-) -> MagicMock:
+    mocker,
+    aws_static_costs,
+    aws_account_names,
+):
     mocked_client = mocker.MagicMock()
     mocked_client.get_cost_and_usage.side_effect = [
         {
@@ -86,8 +79,6 @@ def mocked_boto3_client(
     return mocked_client
 
 
-def test_aws_cost_report(
-    expected_cost_report_message: str, mocked_boto3_client: MagicMock
-) -> None:
+def test_aws_cost_report(expected_cost_report_message, mocked_boto3_client):
     actual_cost_report_message = update_cost_reporter()
     assert actual_cost_report_message == expected_cost_report_message
